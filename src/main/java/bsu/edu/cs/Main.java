368deca7d226b33f12c4dbb17275876878274aad
JoshYoder1 <josh.yoder@bsu.edu>
2025-09-18 14:31:32 -0400

    Edit RetrieveArticle.java, Main.java

--- src/main/java/bsu/edu/cs/Main.java
+++ src/main/java/bsu/edu/cs/Main.java
@@ -1,9 +1,16 @@
 package bsu.edu.cs;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        RetrieveArticle retrieveArticle = new RetrieveArticle();
     }//end main method
+
+    public String getArticleName(){
+        return input.nextLine();
+    }//end getArticleName
 
 }//end class
--- src/main/java/bsu/edu/cs/RetrieveArticle.java
+++ src/main/java/bsu/edu/cs/RetrieveArticle.java
@@ -10,9 +10,29 @@ import java.nio.charset.Charset;
 public class RetrieveArticle {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-
+        URLConnection connection = connectToWikipedia("Einstein");
+        String jsonData = readJsonAsStringFrom(connection);
+        printRawJson(jsonData);
     }//end main
 
+    private static URLConnection connectToWikipedia(String articleName) throws IOException, URISyntaxException {
+        String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
+                URLEncoder.encode(articleName, Charset.defaultCharset()) +
+                "&rvprop=timestamp" + URLEncoder.encode("|",Charset.defaultCharset()) + "user&rvlimit=4&redirects";
+        URI uri = new URI(encodedUrlString);
+        URLConnection connection = uri.toURL().openConnection();
+        connection.setRequestProperty("User-Agent",
+                "FirstProject/0.1 (academic use; https://example.com)");
+        connection.connect();
+        return connection;
+    }//end connectToWikipedia
 
+    private static String readJsonAsStringFrom(URLConnection connection) throws IOException {
+        return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+    }//end readJsonAsStringFrom
+
+    private static void printRawJson(String jsonData) {
+        System.out.println(jsonData);
+    }
 
 }//end class
