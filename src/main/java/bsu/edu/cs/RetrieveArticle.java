98ffa994a52a08befe946c71ac15eee92fb1861b
JoshYoder1 <josh.yoder@bsu.edu>
2025-09-18 13:26:54 -0400

    edit retreive Article

--- src/main/java/bsu/edu/cs/RetrieveArticle.java
+++ src/main/java/bsu/edu/cs/RetrieveArticle.java
@@ -1,5 +1,18 @@
 package bsu.edu.cs;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
 public class RetrieveArticle {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+    }//end main
+
+
 
 }//end class
